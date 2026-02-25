505ef14e620866e51ddffdb7693aee5849c85ab5
Solix <81125185+SolixWasTaken@users.noreply.github.com>
2026-02-25 14:35:07 -0600

    Update default key
    Noticed that this key may conflict with default resources like the chat state so changing it to Y seems better.

--- shared/configuration.lua
+++ shared/configuration.lua
@@ -1,7 +1,8 @@
 return {
     enabled = false,            -- One handed weapon holding will be enabled by default if true
     command = 'toggleonehand', -- Command for toggling
-    key = 'L',                 -- Key for toggling
+    key = 'Y',                 -- Key for toggling
     
     clipset = 'move_ped_wpn_jerrycan_generic'
+
 }
